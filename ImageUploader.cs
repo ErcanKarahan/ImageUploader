0b43d4bf84abf16823ad6b7e1fe884bb994bedc4
Ercan Karahan <ercankarahan@hotmail.de>
2021-02-28 01:30:43 +0300

    Add files via upload

---
+++ ImageUploader.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Project.COMMON.MyTools
+{
+    
+
+    public static class ImageUploader
+    {
+        public static string UploadImage(string serverPath, HttpPostedFileBase file)
+        {
+            if (file!=null)
+            {
+                Guid uniqueName = Guid.NewGuid();
+                
+                serverPath = serverPath.Replace("~", string.Empty); 
+
+                string[] fileArray = file.FileName.Split('.'); 
+
+                string extension = fileArray[fileArray.Length - 1].ToLower(); 
+
+                string fileName = $"{uniqueName}.{extension}";
+
+               
+                if (extension=="jpg"||extension=="gif"||extension=="png"||extension=="jpeg")
+                {
+                    if (File.Exists(HttpContext.Current.Server.MapPath(serverPath+fileName)))
+                    {
+                        return "Dosya zaten var";
+                    }
+                    else
+                    {
+                        string filePath = HttpContext.Current.Server.MapPath(serverPath + fileName);
+
+                        file.SaveAs(filePath);
+
+                        return serverPath + fileName;
+
+                    }
+                }
+                else
+                {
+                    return "Arkadasım niye resim secmedin?";
+                }
+            }
+            else
+            {
+                return "Dosya bos";
+            }
+
+           
+        }
+    }
+}
